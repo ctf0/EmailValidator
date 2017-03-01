ff5f06d735b54820f2b9d300c87015541d792f89
Muah <muah003@gmail.com>
2017-03-02 00:36:32 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # MailGunEmailValidator
-validation rule to validate emali through mailgun api
+validation rule to validate emali using [Mailgun Api](http://documentation.mailgun.com/api-email-validation.html).
+
+- register providor
+
+```php
+'providers' => [
+    App\Providers\MailGunValidatorServiceProvider::class,
+]
+```
+
+- add the rule to the validator
+
+```php
+'email' => 'required|email|mailgun',
+```
+
+# ToDo
+
+* [ ] Turn into Package.
---
+++ src/MailGunValidatorServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\ServiceProvider;
+use Validator;
+
+class MailGunValidatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        Validator::extend('mailgun', function ($attribute, $value, $parameters, $validator) {
+            $req = (new Client())->get('https://api.mailgun.net/v3/address/validate', [
+                'query' => [
+                    'address' => $value,
+                    'api_key' => env('MAILGUN_API_KEY', 'pubkey-83a6-sl6j2m3daneyobi87b3-ksx3q29'),
+                ],
+                'auth'   => null,
+            ]);
+
+            return json_decode($req->getBody()->getContents())->is_valid;
+        }, 'This email is invalid');
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->app->singleton(\App\Providers\MailGunValidatorServiceProvider::class);
+    }
+}
